eb3dbc58ec5dd6cf9947fb42f7d2989f879ffb3c
Evgenii Titov <1994etitov@gmail.com>
2020-11-07 11:40:21 +1100

    Further improved augmentation pipeline; Implemented saving of images and txts

--- labelled_imgs_augmenter.py
+++ labelled_imgs_augmenter.py
@@ -1,5 +1,8 @@
 import os
 import argparse
+import cProfile
+import pstats
+import io
 
 import cv2
 from typing import Tuple, Set, Union, List
@@ -14,12 +17,29 @@ AUGMENT_PIPELINE = imgaug.augmenters.Sequential([
 ])
 
 
+def profile(fnc):
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+    return inner
+
+
 def read_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--folder", required=True,
                         help="Path to a folder with image-txt pairs")
     parser.add_argument("-s", "--save_path", required=True,
                         help="Dir where generated images and txts saved")
+    parser.add_argument("--n", type=int, default=1,
+                        help="Number of new augmented images per original")
     return vars(parser.parse_args())
 
 
@@ -33,6 +53,8 @@ def get_img_txt_pair(filenames: Set[str], folder: str) -> Tuple[str, str]:
         txt_path = os.path.join(folder, filename + ".txt")
         if os.path.exists(image_path) and os.path.exists(txt_path):
             yield image_path, txt_path
+        else:
+            print("[INFO]: Not complete pair for:", filename)
 
 
 def read_txt_content(txt_path: str) -> Tuple[bool, Union[None, list]]:
@@ -94,55 +116,126 @@ def convert_darknet_to_human(
     return boxes_out
 
 
-def augment_image(image: np.ndarray, boxes: list) -> Tuple[np.ndarray, list]:
-    bbs = BoundingBoxesOnImage(
-        [
-            BoundingBox(x1=box[1], y1=box[2], x2=box[3], y2=box[4])
-            for box in boxes
-        ],
-        shape=image.shape
-    )
-    aug_image, aug_boxes = AUGMENT_PIPELINE(image=image, bounding_boxes=bbs)
-    aug_coords = [
-        [
-            box.x1_int if box.x1_int > 0 else 1,
-            box.y1_int if box.y1_int > 0 else 1,
-            box.x2_int if box.x2_int > 0 else 1,
-            box.y2_int if box.y2_int > 0 else 1
-        ]
-        for box in aug_boxes
-    ]
-    return aug_image, aug_coords
+def convert_human_to_darknet(boxes: List[list], img: np.ndarray) -> List[list]:
+    img_h, img_w = img.shape[:2]
+    out = []
+    for box in boxes:
+        cls_, left, top, right, bot = box
+        box_centre_x = round(((left + right) // 2) / img_w, 6)
+        box_centre_y = round(((top + bot) // 2) / img_h, 6)
+        width = round((right - left) / img_w, 6)
+        height = round((bot - top) / img_h, 6)
+        assert all(
+            (0.0 <= _ <= 1.0
+             for _ in (box_centre_x, box_centre_y, width, height))
+        ), "[ERROR]: darknet-style coordinates are not in [0.0, 1.0]"
+        out.append([cls_, box_centre_x, box_centre_y, width, height])
+    return out
 
 
+def augment_image(
+        image: np.ndarray,
+        boxes: list,
+        n_of_new_imgs: int,
+        max_tries: int = 10,
+        visibility_thresh: float = 0.4
+) -> Tuple[list, list]:
+    assert max_tries > 0
+    aug_images, aug_coords = [], []
+    generated, tries = 0, 0
+    while generated < n_of_new_imgs:
+        bbs = BoundingBoxesOnImage(
+            [
+                BoundingBox(x1=box[1], y1=box[2], x2=box[3],
+                            y2=box[4], label=box[0])
+                for box in boxes
+            ],
+            shape=image.shape
+        )
+        aug_image, aug_boxes = AUGMENT_PIPELINE(image=image,
+                                                bounding_boxes=bbs)
+        aug_coord = []
+        for aug_box in aug_boxes:
+            # Check if image happens to be completely out after augmentation
+            if not aug_box.is_partly_within_image(aug_image):
+                continue
+            box_area_total = aug_box.area
+            # Click out any bb sections that are beyond the image
+            aug_box = aug_box.clip_out_of_image(aug_image)
+            box_area_visible = aug_box.area
+            # If only a small part of bb is visible, skip this bb
+            if box_area_visible / box_area_total < visibility_thresh:
+                continue
+            aug_coord.append([
+                aug_box.label, aug_box.x1_int, aug_box.y1_int,
+                aug_box.x2_int, aug_box.y2_int
+            ])
+
+        if len(aug_coord):
+            aug_images.append(aug_image)
+            aug_coords.append(aug_coord)
+            generated += 1
+        else:
+            if tries == max_tries:
+                print(f"Failed to generate a valid image in {tries} attempts")
+                generated += 1
+            else:
+                tries += 1
+
+    return aug_images, aug_coords
+
+
+#@profile
 def main() -> None:
     args = read_args()
     if not os.path.exists(args["save_path"]):
         os.mkdir(args["save_path"])
         print("Dir to store results created:", args["save_path"])
     cv2.namedWindow("", cv2.WINDOW_NORMAL)
+
     filenames = get_unique_names(args["folder"])
     for img_path, txt_path in get_img_txt_pair(filenames, args["folder"]):
-        print("Augmentating image:", os.path.basename(img_path))
+        print("Augmenting image:", os.path.basename(img_path))
         success_img, image = read_image(img_path)
         suceess_txt, coords = read_txt_content(txt_path)
         if not all((success_img, suceess_txt)):
             continue
         coords_human = convert_darknet_to_human(coords, image)
-
         # draw_boxes(image, coords_human)
-        # cv2.imshow("ORIGINAL", image)
+        # cv2.imshow("", image)
         # cv2.waitKey(0)
+        # continue
+        aug_images, aug_boxes = augment_image(
+            image, coords_human, n_of_new_imgs=args["n"]
+        )
+        former_name = os.path.splitext(os.path.basename(img_path))[0]
 
-        aug_image, aug_boxes = augment_image(image, coords_human)
+        # for image_, boxes in zip(aug_images, aug_boxes):
+        #     draw_boxes(image_, boxes)
+        #     cv2.imshow("", image_)
+        #     cv2.waitKey(0)
 
-        # print("Aug boxes:", aug_boxes)
-        # draw_boxes(aug_image, aug_boxes)
-        # cv2.imshow("", aug_image)
-        # cv2.waitKey(0)
-
-        # TODO: Save images (run some coordinate check, like that its
-        #       area > 1, else its a line and something went wrong
+        if len(aug_images):
+            for i, (image_, boxes) in enumerate(zip(aug_images, aug_boxes)):
+                new_name = f"{former_name}_{i}"
+                boxes_darknet = convert_human_to_darknet(boxes, image_)
+                print(f"Name: {new_name}. Darknet coords: {boxes_darknet}")
+                txt_save_path = os.path.join(
+                    args["save_path"], new_name + ".txt"
+                )
+                try:
+                    # Save image
+                    cv2.imwrite(
+                        os.path.join(args["save_path"], new_name + ".jpg"),
+                        image_
+                    )
+                    # Save txt
+                    with open(txt_save_path, "w") as file:
+                        for box_darknet in boxes_darknet:
+                            file.write(" ".join([str(e) for e in box_darknet]))
+                except Exception as e:
+                    print(f"Failed to save augmented pair img-txt. Error: {e}")
+                    continue
 
 
 if __name__ == "__main__":
